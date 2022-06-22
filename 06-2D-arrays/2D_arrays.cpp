6c42c9f322f7b9809943b9d6a3be3dfa0f8880bc
Irfan Habib <90855993+irfanhabibglobal@users.noreply.github.com>
2022-06-22 10:54:12 +0530

    Update 2D_arrays.cpp

--- 06-2D-arrays/2D_arrays.cpp
+++ 06-2D-arrays/2D_arrays.cpp
@@ -1,18 +1,6 @@
 #include<iostream>
+#include<climits>
 using namespace std;
-
-//Find if an element exist in the array
-bool isPresent(int arr[][3], int target, int row, int col){
-    
-    for(int row=0;row<3;row++){
-        for(int col=0;col<3;col++){
-            if(arr[row][col]==target){
-                return 1;
-            }
-        }
-    }
-    return 0;
-}
 
 //Take row wise 2D array input
 int inputArr(int a[][3], int row, int col){
@@ -34,6 +22,19 @@ int printArr(int a[][3], int row, int col){
         }
         cout<<endl;
     }
+}
+
+//Find if an element exist in the array
+bool isPresent(int arr[][3], int target, int row, int col){
+    
+    for(int row=0;row<3;row++){
+        for(int col=0;col<3;col++){
+            if(arr[row][col]==target){
+                return 1;
+            }
+        }
+    }
+    return 0;
 }
 
 //Sum of array row wise
@@ -60,6 +61,25 @@ void printColSum(int a[][3], int row, int col){
     }
 }
 
+int largestRowSum(int arr[][3], int row, int col){
+    
+    int maxi = INT_MIN;
+    int rowIndex = -1;
+    
+    for(int row=0; row<3; row++){
+        int sum=0;
+        for(int col=0; col<3; col++){
+            sum+=arr[row][col];
+        }
+        if(sum>maxi){
+            maxi = sum;
+            rowIndex = row;
+        }
+    }
+    cout<<"The maximum sum is: "<<maxi<<endl;
+    return rowIndex;
+}
+
 int main(){
     
     int arr[3][3];
@@ -81,6 +101,11 @@ int main(){
     
     printRowSum(arr,3,3);
     printColSum(arr,3,3);
+    
+    int ansIndexRow = largestRowSum(arr,3,3);
+    cout<<"Max row is at index: "<<ansIndexRow<<endl;
+    int ansIndexCol = largestRowSum(arr,3,3);
+    cout<<"Max col is at index: "<<ansIndexCol<<endl;
     
     return 0;
 }

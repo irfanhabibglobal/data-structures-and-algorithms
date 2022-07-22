dcc5720799a029f9002ef0148d0b7f8f97fdeeec
Irfan Habib <90855993+irfanhabibglobal@users.noreply.github.com>
2022-07-22 06:42:11 +0530

    Create Pattern1.cpp

---
+++ 00-basic-programs/patterns/Pattern1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int x;
+    cin >> x;
+    
+    int i = 1;
+    while(i<=c){
+        
+        int j=1;
+        while(j<=c){
+            cout<<'*';
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
